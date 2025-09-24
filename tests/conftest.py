cd53c34ab2596b16cf93823aa5d9be8ae8c15b63
Michael Foster <m.foster@sheffield.ac.uk>
2025-09-24 16:15:44 +0100

    Added initial_branch to git repos for CI tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,7 +18,7 @@ def fixture_flaky_triangle_repo(tmpdir_factory):
     Fixture for a minimal git repo with a commit history to hide failing tests.
     """
     repo_root = tmpdir_factory.mktemp("flaky_triangle_repo")
-    repo = git.Repo.init(repo_root)
+    repo = git.Repo.init(repo_root, initial_branch="main")
     shutil.copy(os.path.join("tests", "resources", "triangle_example.txt"), os.path.join(repo_root, "triangle.py"))
     repo.index.add(["triangle.py"])
     repo.index.commit("Initial commit of test file.")
@@ -37,7 +37,7 @@ def fixture_deflaker_repo(tmpdir_factory):
     Fixture for a minimal git repo with a commit history of broken tests.
     """
     repo_root = tmpdir_factory.mktemp("deflaker_repo")
-    repo = git.Repo.init(repo_root)
+    repo = git.Repo.init(repo_root, initial_branch="main")
     shutil.copy(os.path.join("tests", "resources", "deflaker_example.txt"), os.path.join(repo_root, "app.py"))
     repo.index.add(["app.py"])
     repo.index.commit("Initial commit of test file.")
